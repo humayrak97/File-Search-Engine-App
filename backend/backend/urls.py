a142b767aaef3176febf233e695a3f839fb8dd4a
Alif Elham <alif.khan1@northsouth.edu>
2021-11-03 23:31:22 +0600

    Created a django app search_engine. Tried url routing.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#this tells the browser that if the url is extended to 'page/' then it takes us to that page
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
