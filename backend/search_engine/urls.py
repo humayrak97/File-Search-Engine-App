1937b91c2cae265ca8bd2d90bcc7eba64c29674c
Mohammad Junayed Hasan <mohammad.hasan5@northsouth.edu>
2021-11-08 01:43:45 +0600

    Fixed merge problems. Updated backend, connected sign up form with database.

--- backend/search_engine/urls.py
+++ backend/search_engine/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf.urls import include
 
 urlpatterns = [
-    path('login/', views.login, name='search_engine-login'),
+    path('', views.login, name='search_engine-login'),
     path('signup/', views.signup, name='search_engine-signup'),
     path('accountRecovery/', views.accountRecovery, name='search_engine-accountRecovery'),
 ]
--- backend/search_engine/views.py
+++ backend/search_engine/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+
 # Create your views here.
 
 def login(request):
     return render(request, 'search_engine/login.html', {'title': 'Login'})
 
-def signup(request):
-    return render(request, 'search_engine/signup.html', {'title': 'Signup'})
-
 def accountRecovery(request):
     return render(request, 'search_engine/recoveryPass.html', {'title': 'accountRecovery'})
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        if pass1 == pass2:
+            if User.objects.filter(email=email).exists():
+                messages.success(request, 'An account with that email already exists.')
+                return redirect('search_engine-signup')
+            else:
+                user = User.objects.create_user(email = email, password=pass1)
+                user.Save();
+                return redirect('search_engine-login')
+
+        else:
+            messages.info(request, 'The passwords do not match.')
+
+
+    return render(request, 'search_engine/signup.html', {'title': 'Signup'})
