34a9df45a9d1f5025f3bee0917e4bdbcac89eac3
Alif Elham <alif.khan1@northsouth.edu>
2021-11-26 23:35:55 +0600

    Worked on crawler to scrape urls

--- backend/crawling/crawling/spiders/fetchData.py
+++ backend/crawling/crawling/spiders/fetchData.py
@@ -1,13 +1,24 @@
 import scrapy
+import textract
+from itertools import chain
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+
+#allowing extensions 
+TEXTRACT_EXTENSIONS = [".pdf", ".doc", ".docx", ""]
+
+#custom link extractor
+class CustomLinkExtractor(LinkExtractor):
+    def __init__(self, *args, **kwargs):
 
 #ContentSpider subclasses scrapy.Spider
 class ContentSpider(scrapy.Spider):
      name = "content"  #spider
 
      def start_requests(self):
-         urls = [
-             
+         urls = [	#urls to be crawled 
+             'https://www.imagescape.com/media/uploads/zinnia/2018/08/20/scrape_me.html', 
+	     'https://www.northsouth.edu',
          ]
          for url in urls:
              yield scrapy.Request(url=url, callback=self.parse)
@@ -16,7 +27,8 @@ class ContentSpider(scrapy.Spider):
      def parse(self, response):
          for link in self.link_extractor.extract_links(response):
              #yield Request(link.url, callback=self.parse)
-             with open('scraped.txt','wb') as f: 
+	     #writing the scraped content in the text file
+             with open('scraped.txt','wb') as f:  
                  f.write(response.url.upper())
                  f.write("\n")
                  f.write(extracted_data)
