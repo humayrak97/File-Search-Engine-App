57938192b613eb75e24bc1ad8cf981c03cc33301
Mohammad Junayed Hasan <mohammad.hasan5@northsouth.edu>
2021-11-28 22:11:45 +0600

    Updated user profile page, added edit profile option and add image option.

--- backend/search_engine/models.py
+++ backend/search_engine/models.py
@@ -1,14 +1,5 @@
 from django.db import models
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
-
-class People(UserCreationForm):
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- backend/search_engine/views.py
+++ backend/search_engine/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from search_engine.models import People
-from django.contrib.auth.models import User, auth
+from search_engine.forms import People, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
@@ -71,4 +71,23 @@ def about(request):
 
 @login_required
 def user(request):
-    return render(request, 'search_engine/user.html', {'title': 'user'})
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('search_engine-user')
+        
+    
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'search_engine/user.html', context)
